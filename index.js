bf871b2d2ca31f460f8c0aa7f8150d2da97bf26d
Arham Siddiqi <arhsid6501@gmail.com>
2022-09-23 18:15:09 -0500

    updated hw3

--- index.js
+++ index.js
@@ -9,69 +9,20 @@ app.set('view engine', 'pug')
 
 
 // database
-const {Client} = require('pg')
-const client = new Client({
-    host : 'localhost',
-    user : 'postgres',
-    port : 5432,
-    password : 'Asym_111',
-    database: 'postgres'
-});
-
-client.connect();
-  var ver;
-  const response = client.query('SELECT VERSION()', (err, res)=>{
-    if(!err){
-        console.log(res.rows);
-        ver = res.rows;
-    }
-    else{
-        console.log(err.message);
-    }
-    client.end();
-  })
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://postgres:Asym_111@localhost:5432/postgres')
 
   app.get('/', function(req, res) {
-    //res.render('index');
-    res.send(ver);
-  });
-
-/* const Pool = require('pg').Pool
-
-var connection = null;
-if (process.env.DATABASE_URL != null) {
-    connection = {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
-    }
-} else {
-    connection = {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'postgres',
-        password: 'galaxy88',
-        port: 5432
-    }
-}
-
-const pool = new Pool(connection)
-
-app.get ('/', function(req, res) {
-    // res.render('index', {
-    //     title: "Pearleen Tran's HW3"
-    // })
-
-    pool.query('SELECT VERSION()', (err, version_results) => {
-        if (err) {
-          return console.error('Error executing query', err.stack)
-        }
-
+    db.one('SELECT VERSION();').then(data => {
         res.render('index', {
-            title: "Pearleen Tran's HW3",
-            databaseVer: version_results.rows[0].version
+            version: data.version
         })
-    })
-}) */
+        console.log('DATA:', data.version)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+  });
 
 app.listen(3000, () => {
     console.log('App listening on port 3000')
